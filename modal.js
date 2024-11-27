a522aab01fd822d3df6d28fcdde57b6f6e74e2a4
Emeric <emeric.pataut@gmail.com>
2024-11-27 11:15:18 +0100

    feat(modal): display projects in the modal

--- modal.js
+++ modal.js
@@ -1,7 +1,51 @@
 // Fenetre modale
 
+import { projectsData } from "./gallery.js";
+
+
+// Fonction pour afficher les projets dans la modale
+function displayProjectsInModal(Projects) {
+  const modalGallery = document.querySelector('.modal-gallery');
+  modalGallery.innerHTML="";
+
+  Projects.forEach(project => {
+    const projectElement = document.createElement('figure');
+    projectElement.className = 'modal-project';
+
+    const img = document.createElement('img');
+    img.src = project.imageUrl;
+    img.alt = project.title;
+
+
+    // Ajout 'bouton' icon "delete"
+    const iconContainer = document.createElement('div');
+    iconContainer.className = 'icon-container';
+    // Ajout icon "trash"
+    const deleteIcon = document.createElement('i');
+    deleteIcon.className = 'fa-solid fa-trash-can delete-icon';
+
+    iconContainer.appendChild(deleteIcon);
+
+    projectElement.appendChild(img);
+    projectElement.appendChild(iconContainer);
+
+    modalGallery.appendChild(projectElement);
+
+  });
+}
+
+
+
+
+
+
 // Bouton "modifier" pour l'ouverture de la modale
 const openModalButton = document.querySelector('.edit-link');
+
+if (!openModalButton) {
+  console.error("Le bouton 'Modifier' n'a pas été trouvé dans le DOM.");
+} 
+
 
 // Fonction pour créer et afficher la modale
 function createModal() {
@@ -41,13 +85,23 @@ function createModal() {
     // Ajout de l'overlay au document
     document.body.appendChild(overlay);
   
-    // Gestion des événements de fermeture
+    // Gestion de la fermeture
     closeModalButton.addEventListener('click', () => document.body.removeChild(overlay));
     overlay.addEventListener('click', (e) => {
       if (e.target === overlay) document.body.removeChild(overlay);
     });
+
+
+    // Appeler la fonction "displayProjectsInModal"
+    displayProjectsInModal(projectsData);
+
+
   }
   
+
+
+
+
   // Ajouter un événement au bouton pour afficher la modale
   openModalButton.addEventListener('click', createModal);
 
